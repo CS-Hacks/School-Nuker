d129d0d5838c06c173778f71043778ad3cd9702b
CS-Hacks <117430100+CS-Hacks@users.noreply.github.com>
2022-11-15 11:48:22 +0530

    Rename file

---
+++ slow_hanger.py
@@ -0,0 +1,19 @@
+planet = "32 ascii char string (=32 bytes)" 
+blackhole = [] 
+blackhole1 = [] 
+blackhole2 = [] 
+blackhole3 = [] 
+blackhole4 = [] 
+blackhole5 = [] 
+blackhole6 = [] 
+blackhole7 = [] 
+print("Hello")
+while True: 
+    blackhole.append(planet)
+    blackhole1.append(planet)
+    blackhole2.append(planet)
+    blackhole3.append(planet)
+    blackhole4.append(planet)
+    blackhole5.append(planet)
+    blackhole6.append(planet)
+    blackhole7.append(planet)
